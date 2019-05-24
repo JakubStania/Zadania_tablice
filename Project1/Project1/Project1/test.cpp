60822c2f0e15ba99f702805a57d9f746dc2f7f53
StaniaJakub <kubastania@gmail.com>
2019-05-24 14:47:19 +0200

    cwiczenie10/66 gacus

---
+++ Project1/Project1/Project1/test.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int spacje = 0;
+
+
+char Zliczanie()
+{
+	for (int i = 0; i < j; i++)
+	{
+		if (char tekst[j] = " ")
+		{
+			
+			spacje++;
+			return spacje;
+		}
+	}
+
+}
+int main()
+{
+	char j;
+	cin >> j;
+	Zliczanie();
+	cout << "liczba spacji: " <<spacje<<endl ;
+	system("pause");
+}
